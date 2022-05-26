19fca4155c6c9d1a96d991c6cdd3a692fcb99cb4
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-26 19:49:49 -0300

    final da tarefa

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é seu nome ?");
+let sobrenome = prompt("Qual é seu sobrenome ?");
+
+alert(`Nome Completo: ${nome} ${sobrenome}`);
+alert(`Nome Catalogo: ${sobrenome.toUpperCase()} ${nome}`);
